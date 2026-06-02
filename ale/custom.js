ecc24423ae85750f0bfc88afb3d61028c34ca6d5
Alex <investmentswayle@yahoo.com>
2026-06-02 17:37:49 +0200

    header e sezioni

--- ale/custom.js
+++ ale/custom.js
@@ -1,6 +1,6 @@
 var CSbody = document.querySelector("body");
-const CSnavbarMenu = document.querySelector("#cs-navigation");
-const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+const CSnavbarMenu = document.querySelector("#header");
+const CShamburgerMenu = document.querySelector("#header .cs-toggle");
 
 CShamburgerMenu.addEventListener('click', function() {
     CShamburgerMenu.classList.toggle("cs-active");
@@ -20,19 +20,27 @@ function ariaExpanded() {
     }
 }
 
-document.addEventListener('scroll', (e) => { 
-    const scroll = document.documentElement.scrollTop;
-    if(scroll >= 100){
-document.querySelector('body').classList.add('scroll')
-    } else {
-    document.querySelector('body').classList.remove('scroll')
-    }
-});
+const dropDowns = Array.from(document.querySelectorAll('#header .cs-dropdown'));
+for (const item of dropDowns) {
+    const onClick = () => {
+    item.classList.toggle('cs-active')
+}
+item.addEventListener('click', onClick)
+}
 
-const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-    for (const item of dropDowns) {
-        const onClick = () => {
-        item.classList.toggle('cs-active')
+//modal
+const modal = document.getElementById("contactModal");
+
+function openModal() {
+    modal.classList.add("show");
+}
+
+function closeModal() {
+    modal.classList.remove("show");
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+    closeModal();
     }
-    item.addEventListener('click', onClick)
-    }
+}
